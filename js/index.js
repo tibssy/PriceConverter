5b8c76e4466f93e68e40a31588150f64c424d7dd
tibssy <norbert.tiborcz@gmail.com>
2020-10-13 15:33:15 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+function openCvReady() {
+  cv["onRuntimeInitialized"] = () => {
+    let img = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    let roi = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    let roi1 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    let gray = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    let cap = new cv.VideoCapture(video);
+
+    const FPS = 30;
+    function processVideo() {
+      let begin = Date.now();
+      cap.read(img);
+      let rect = new cv.Rect(200, 150, 400, 300);
+      let rect1 = new cv.Rect(45, 60, 310, 180);
+      roi = img.roi(rect);
+      roi1 = roi.roi(rect1);
+      cv.cvtColor(roi1, gray, cv.COLOR_RGBA2GRAY);
+
+      cv.imshow("canvasRoi", roi);
+      cv.imshow("canvasgray", gray);
+      let delay = 1000 / FPS - (Date.now() - begin);
+      setTimeout(processVideo, delay);
+    }
+
+    setTimeout(processVideo, 0);
+  };
+}
+
+// the code starts here
+let video = document.getElementById("videoInput");
+navigator.mediaDevices
+  .getUserMedia({ video: { facingMode: "environment" }, audio: false })
+  .then(function (stream) {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(function (err) {
+    console.log("An error occurred! " + err);
+  });
+
+openCvReady();
+
+//Set.prototype.add(value)
