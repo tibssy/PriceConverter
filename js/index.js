61149566b0f041acab3082ee227e0193e19a6939
tibssy <norbert.tiborcz@gmail.com>
2020-10-14 17:08:36 +0100

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,19 +1,17 @@
 function openCvReady() {
   cv["onRuntimeInitialized"] = () => {
     let img = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    let roi = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     let gray = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let cap = new cv.VideoCapture(video);
 
     const FPS = 30;
     function processVideo() {
       let begin = Date.now();
       cap.read(img);
-      let rect = new cv.Rect(100, 120, 600, 360);
-      roi = img.roi(rect);
-      cv.cvtColor(roi, gray, cv.COLOR_RGBA2GRAY);
-
-      cv.imshow("canvasgray", gray);
+      cv.cvtColor(img, gray, cv.COLOR_RGBA2GRAY);
+      cv.threshold(gray, dst, 130, 255, cv.THRESH_BINARY);
+      cv.imshow("canvasgray", dst);
       let delay = 1000 / FPS - (Date.now() - begin);
       setTimeout(processVideo, delay);
     }
