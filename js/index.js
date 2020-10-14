d8e0550728cc02770f4fc9cc431258a0cc6b89d7
tibssy <norbert.tiborcz@gmail.com>
2020-10-14 17:37:44 +0100

    Initial commit

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ function openCvReady() {
   cv["onRuntimeInitialized"] = () => {
     let img = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     let gray = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    let thres = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    let M = cv.Mat.eye(3, 3, cv.CV_32FC1);
+    let anchor = new cv.Point(-1, -1);
     let cap = new cv.VideoCapture(video);
 
     const FPS = 30;
@@ -10,7 +13,8 @@ function openCvReady() {
       let begin = Date.now();
       cap.read(img);
       cv.cvtColor(img, gray, cv.COLOR_RGBA2GRAY);
-      cv.threshold(gray, dst, 130, 255, cv.THRESH_BINARY);
+      cv.threshold(gray, thres, 130, 255, cv.THRESH_BINARY);
+      cv.filter2D(thres, dst, cv.CV_8U, M, anchor, 0, cv.BORDER_DEFAULT);
       cv.imshow("canvasgray", dst);
       let delay = 1000 / FPS - (Date.now() - begin);
       setTimeout(processVideo, delay);
