289e57993584f0049078c626fb142f0a19e818f9
tibssy <norbert.tiborcz@gmail.com>
2020-10-15 23:26:26 +0100

    Initial commit

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ function openCvReady() {
     let img = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     let gray = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let thres = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    let blure = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let M = cv.Mat.eye(3, 3, cv.CV_32FC1);
+    let ME = cv.Mat.ones(5, 5, cv.CV_8U);
     let anchor = new cv.Point(-1, -1);
     let cap = new cv.VideoCapture(video);
 
@@ -14,7 +16,8 @@ function openCvReady() {
       cap.read(img);
       cv.cvtColor(img, gray, cv.COLOR_RGBA2GRAY);
       cv.threshold(gray, thres, 130, 255, cv.THRESH_BINARY);
-      cv.filter2D(thres, dst, cv.CV_8U, M, anchor, 0, cv.BORDER_DEFAULT);
+      cv.filter2D(thres, blure, cv.CV_8U, M, anchor, 0, cv.BORDER_DEFAULT);
+      cv.erode(blure, dst, ME, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
       cv.imshow("canvasgray", dst);
       let delay = 1000 / FPS - (Date.now() - begin);
       setTimeout(processVideo, delay);
