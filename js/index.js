d62b39a0dc8c067cb282db53b4913d687aaed3d0
tibssy <norbert.tiborcz@gmail.com>
2020-11-10 14:51:08 +0000

    median blur

--- js/index.js
+++ js/index.js
@@ -3,15 +3,11 @@ function openCvReady() {
     let img = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     let gray = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let thresh = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-    let blure = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let label = new cv.Mat();
     let stats = new cv.Mat();
     let centroids = new cv.Mat();
     let resolution = [img.cols, img.rows];
-    let M = cv.Mat.eye(3, 3, cv.CV_32FC1);
-    let ME = cv.Mat.ones(5, 5, cv.CV_8U);
-    let anchor = new cv.Point(-1, -1);
     let cap = new cv.VideoCapture(video);
 
     const FPS = 30;
@@ -20,8 +16,7 @@ function openCvReady() {
       cap.read(img);
       cv.cvtColor(img, gray, cv.COLOR_RGBA2GRAY);
       cv.threshold(gray, thresh, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU);
-      cv.filter2D(thresh, blure, cv.CV_8U, M, anchor, 0, cv.BORDER_DEFAULT);
-      cv.erode(blure, dst, ME, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+      cv.medianBlur(thresh, dst, 5);
       cv.connectedComponentsWithStats(dst, label, stats, centroids, 8, cv.CV_32S);
 
       let res = prepare_stats(stats);
@@ -38,7 +33,7 @@ function openCvReady() {
 
 
       cv.imshow("canvasvideo", img);
-      //  cv.imshow("canvasgray", dst);
+      cv.imshow("canvasgray", dst);
       let delay = 1000 / FPS - (Date.now() - begin);
       setTimeout(processVideo, delay);
     }
